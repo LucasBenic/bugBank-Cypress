7af4e972dd216f68fa34cf7de9f43a397098a1b0
LucasBenic <lprodocio22@gmail.com>
2023-12-19 19:32:47 -0300

    fix-config

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,18 @@
-const { defineConfig } = require("cypress");
+// cypress/config.js
 
-module.exports = defineConfig({
-
-  env: {
-    PASS_BUG: Cypress.env('PASS_BUG'),
-  },
-  
-  e2e: {
-    viewportHeight: 1080,
-    viewportWidth: 1920,
-
-    baseUrl: 'https://bugbank.netlify.app/',
-
-    setupNodeEvents(on, config) {
+module.exports = (on, config) => {
+  // Your Cypress configuration logic here
+  return {
+    env: {
+      PASS_BUG: Cypress.env('PASS_BUG'),
     },
-  },
-});
+    e2e: {
+      viewportHeight: 1080,
+      viewportWidth: 1920,
+      baseUrl: 'https://bugbank.netlify.app/',
+      setupNodeEvents(on, config) {
+        // Additional configuration if needed
+      },
+    },
+  };
+};
