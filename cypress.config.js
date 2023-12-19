38cf338b5d19de8de3cbbaa704cbaf2b55915f71
LucasBenic <lprodocio22@gmail.com>
2023-12-19 19:51:08 -0300

    added-env

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,10 @@ module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
 
+  env: {
+    PASS_BUG: '',
+  },
+
   e2e: {
     baseUrl: 'https://bugbank.netlify.app/',
     setupNodeEvents(on, config) {
@@ -12,43 +16,3 @@ module.exports = defineConfig({
     },
   },
 });
-
-
-
-
-
-
-// module.exports = defineConfig({
-//   projectId: "e9msfd",
-//   defaultCommandTimeout: 10000,
-//   retries: 0,
-//   viewportWidth: 1920,
-//   viewportHeight: 1200,
-
-//   env: {
-//     local: {
-//       CMS: "http://localhost:3030/",
-//       broker: "http://localhost:3050/",
-//       organization: "http://localhost:3040/"
-//     },
-//     stage: {
-//       CMS: "https://dev-cms.dashre.us/",
-//       broker: "https://dev-broker.dashre.us/",
-//       organization: "https://dev-organization.dashre.us/"
-//     }
-//   },
-
-//   e2e: {
-//     setupNodeEvents(on, config) {
-//       on('task', {
-//         listFilesInFolder(folderPath) {
-//           const fs = require('fs');
-//           const path = require('path');
-//           const folderAbsolutePath = path.join(config.projectRoot, folderPath);
-//           const files = fs.readdirSync(folderAbsolutePath);
-//           return files;
-//         },
-//       });
-//     },
-//   },
-// });
