bb2d3ba6a6dd6479a401b3d7955b7af3c45efe73
LucasBenic <lprodocio22@gmail.com>
2023-12-19 19:27:37 -0300

    added-env

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  env: {
+    PASS_BUG: Cypress.env('PASS_BUG'),
+  },
+  
   e2e: {
     viewportHeight: 1080,
     viewportWidth: 1920,
