5e743df119c55a7914216a581850a10d3100aafa
Lucas Roberto Benicio Prodocio <128319082+LucasBenic@users.noreply.github.com>
2024-02-26 10:59:58 -0300

    Update login.cy.js

--- cypress/e2e/login/login.cy.js
+++ cypress/e2e/login/login.cy.js
@@ -1,8 +1,13 @@
 import { qase } from 'cypress-qase-reporter/dist/mocha'
 describe('Exemplo', () => {
+
+  const password = Cypress.env('PASS_BUG')
   qase(5, 
     it('Validate The Ability To Search For A Product', () => {
       cy.visit('www.google.com')
-      cy.get('input')
+      cy.get('input[type="string"]').type('lprodocio22@gmail.com')
+      cy.get('input[type="password"]').type(password, {log: false})
+      cy.get('button:contains("Log in")').click()
+      cy.contains('button', 'Home').should('exist')
     }))
 })
